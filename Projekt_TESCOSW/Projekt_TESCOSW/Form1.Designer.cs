d99f879414822edeffb3451643720534197b2937
Skriprc <146934296+Skriprc@users.noreply.github.com>
2024-04-30 20:03:15 +0200

    Add files via upload

---
+++ Projekt_TESCOSW/Projekt_TESCOSW/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace Projekt_TESCOSW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(531, 256);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog";
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(447, 285);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(96, 22);
+            this.button.TabIndex = 1;
+            this.button.Text = "Zvolit Soubor";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 315);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button;
+    }
+}
+
---
+++ Projekt_TESCOSW/Projekt_TESCOSW/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Projekt_TESCOSW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            //Vybrání souboru
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "XML files (*.xml)|*.xml|All files (*.*)|*.*"; //Filtr, aby byl soubor .xml
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                //Načtení souboru
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(filePath);
+
+                //Rozdělí data, vypočítá celkovou cenu a celkovou cenu s dph pro každé prodané auto (viz ParseXml)
+                List<Sale> sales = ParseXml(xmlDoc);
+
+                //Rozdělení prodaných aut podle názvu (viz AggregateSales)
+                var aggregatedSales = AggregateSales(sales);
+
+                //Přidání nových dat do tabulky (viz PopulateGrid)
+                PopulateGrid(aggregatedSales);
+            }
+        }
+
+        private List<Sale> ParseXml(XmlDocument xmlDoc)
+        {
+            List<Sale> sales = new List<Sale>();
+
+            //Rozdělení a přiřazení dat
+            XmlNodeList saleNodes = xmlDoc.SelectNodes("/autosalon/car");
+            foreach (XmlNode saleNode in saleNodes)
+            {
+                string name = saleNode.SelectSingleNode("Name").InnerText;
+                DateTime dateofsale = DateTime.Parse(saleNode.SelectSingleNode("Dateofsale").InnerText);
+                double price = double.Parse(saleNode.SelectSingleNode("Price").InnerText);
+                double dph = double.Parse(saleNode.SelectSingleNode("DPH").InnerText);
+
+                //Přepočítání ceny s dph
+                double dph_price = price + (price * (dph / 100));
+
+                //Přidá prodané auto do seznamu
+                sales.Add(new Sale { Name = name, DateOfSale = dateofsale, Price = price, DphPrice = dph_price });
+            }
+
+            return sales;
+        }
+
+        private Dictionary<string, (int Count, double TotalPrice, double TotalPriceDPH)> AggregateSales(List<Sale> sales)
+        {
+            Dictionary<string, (int Count, double TotalPrice, double TotalPriceDPH)> aggregatedSales = new Dictionary<string, (int, double, double)>();
+
+            //Pro každé prodané auto v seznamu
+            foreach (Sale sale in sales)
+            {
+                if (aggregatedSales.ContainsKey(sale.Name))
+                {
+                    (int Count, double TotalPrice, double TotalPriceDPH) data = aggregatedSales[sale.Name];
+                    aggregatedSales[sale.Name] = (data.Count + 1, data.TotalPrice + sale.Price, data.TotalPriceDPH + sale.DphPrice);
+                }
+                else
+                {
+                    aggregatedSales.Add(sale.Name, (1, sale.Price, sale.DphPrice));
+                }
+            }
+
+            return aggregatedSales;
+        }
+
+        private void PopulateGrid(Dictionary<string, (int Count, double TotalPrice, double TotalPriceDPH)> aggregatedSales)
+        {
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("Název", typeof(string));
+            dataTable.Columns.Add("Počet prodaných vozů", typeof(int));
+            dataTable.Columns.Add("Celková cena", typeof(string));
+            dataTable.Columns.Add("Celková cena s DPH", typeof(string));
+
+            foreach (var kvp in aggregatedSales)
+            {
+                string adjustedPriceFormatted = kvp.Value.TotalPriceDPH.ToString("N2");
+                string totalPriceFormatted = kvp.Value.TotalPrice.ToString("N2");
+
+
+                dataTable.Rows.Add(kvp.Key, kvp.Value.Count, totalPriceFormatted, adjustedPriceFormatted);
+            }
+
+            dataGridView.DataSource = dataTable;
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+    }
+
+    class Sale
+    {
+        public string Name { get; set; }
+        public DateTime DateOfSale { get; set; }
+        public double Price { get; set; }
+        public double DphPrice { get; set; }
+    }
+}
+
